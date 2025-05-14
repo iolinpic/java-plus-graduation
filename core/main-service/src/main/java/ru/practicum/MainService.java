5b86a9bc40011c26a4495fabf2ab228e7d58a569
iolinpic <iolinpic@gmail.com>
2025-05-14 07:22:53 +0300

    change default settings

--- core/main-service/src/main/java/ru/practicum/MainService.java
+++ core/main-service/src/main/java/ru/practicum/MainService.java
@@ -2,8 +2,12 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class MainService {
 
     public static void main(String[] args) {
