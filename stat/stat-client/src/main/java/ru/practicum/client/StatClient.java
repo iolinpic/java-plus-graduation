e423f51e9224301bdcb7075edfe972ee7279fcc1
iolinpic <iolinpic@gmail.com>
2025-05-14 07:24:15 +0300

    modify stat-client for service-discovery

--- stat/stat-client/src/main/java/ru/practicum/client/StatClient.java
+++ stat/stat-client/src/main/java/ru/practicum/client/StatClient.java
@@ -3,9 +3,14 @@ package ru.practicum.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.MaxAttemptsRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -20,22 +25,50 @@ import java.util.List;
 @Component
 public class StatClient {
 
-    private final String http = "http";
+    private final DiscoveryClient discoveryClient;
+    private RetryTemplate retryTemplate;
 
-    private String serverUri;
-    private RestClient restClient;
+
+    private final RestClient restClient;
 
     @Autowired
-    public StatClient(@Value("${stats-server.uri:http://localhost:9090}") String serverUri) {
-        this.serverUri = serverUri;
+    public StatClient(@Autowired DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
         this.restClient = RestClient.create();
+        this.retryTemplate = new RetryTemplate();
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(3000L);
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        MaxAttemptsRetryPolicy retryPolicy = new MaxAttemptsRetryPolicy();
+        retryPolicy.setMaxAttempts(3);
+        retryTemplate.setRetryPolicy(retryPolicy);
     }
+
+
+    private ServiceInstance getInstance() {
+        String serviceId = "stat-server";
+        try {
+            return discoveryClient
+                    .getInstances(serviceId)
+                    .getFirst();
+        } catch (Exception exception) {
+            throw new RuntimeException(
+                    "Ошибка обнаружения адреса сервиса статистики с id: " + serviceId,
+                    exception
+            );
+        }
+    }
+    private URI makeUri(String path) {
+        ServiceInstance instance = retryTemplate.execute(cxt -> getInstance());
+        return URI.create("http://" + instance.getHost() + ":" + instance.getPort() + path);
+    }
+
 
 
     public void saveHit(EndpointHitDto hitDto) {
         String uri = UriComponentsBuilder.newInstance()
-                .uri(URI.create(serverUri))
-                .path("/hit")
+                .uri(makeUri("/hit"))
                 .toUriString();
 
         restClient.post()
@@ -60,8 +93,7 @@ public class StatClient {
 
     public List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
         String uriWithParams = UriComponentsBuilder.newInstance()
-                .uri(URI.create(serverUri))
-                .path("/stats")
+                .uri(makeUri("/stats"))
                 .queryParam("start", start)
                 .queryParam("end", end)
                 .queryParam("uris", uris)
