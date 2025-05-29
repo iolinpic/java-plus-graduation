dd29a24baf936887556c2af09f9cef2034e89509
iolinpic <iolinpic@gmail.com>
2025-05-29 16:24:28 +0300

    add kafka and new modules

---
+++ stat/serialisation/avro-schemas/src/main/java/ru/practicum/ewm/stats/avro/EventSimilarityDeserializer.java
@@ -0,0 +1,5 @@
+package ru.practicum.ewm.stats.avro;
+
+public class EventSimilarityDeserializer extends BaseAvroDeserializer<EventSimilarityAvro>{
+    public EventSimilarityDeserializer() {super(EventSimilarityAvro.getClassSchema());}
+}
---
+++ stat/serialisation/avro-schemas/src/main/java/ru/practicum/ewm/stats/avro/UserActionDeserializer.java
@@ -0,0 +1,5 @@
+package ru.practicum.ewm.stats.avro;
+
+public class UserActionDeserializer extends BaseAvroDeserializer<UserActionAvro>{
+    public UserActionDeserializer() {super(UserActionAvro.getClassSchema());}
+}
