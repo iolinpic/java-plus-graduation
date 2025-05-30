a52352771960a0738d8b53c810318e374f6d4fd5
iolinpic <iolinpic@gmail.com>
2025-05-30 21:07:13 +0300

    settings modification

--- stats/aggregator/src/main/java/ru/practicum/AggregatorService.java
+++ stats/aggregator/src/main/java/ru/practicum/AggregatorService.java
@@ -3,10 +3,12 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class AggregatorService {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(AggregatorService.class, args);
